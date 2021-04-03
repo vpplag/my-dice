f21bac3bea5192dacc0714d50ec5104e7e5e0cc4
vpplag <74141971+vpplag@users.noreply.github.com>
2021-04-03 23:02:24 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,6 @@
 let Roll = 0
 input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
+    basic.clearScreen()
 })
 input.onGesture(Gesture.Shake, function () {
     Roll = randint(1, 6)
@@ -18,53 +12,45 @@ input.onGesture(Gesture.Shake, function () {
             . . . . .
             . . . . .
             `)
+    } else if (Roll == 2) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (Roll == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (Roll == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (Roll == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
     } else {
-        if (Roll == 2) {
-            basic.showLeds(`
-                . . . . #
-                . . . . .
-                . . . . .
-                . . . . .
-                # . . . .
-                `)
-        } else {
-            if (Roll == 3) {
-                basic.showLeds(`
-                    . . . . #
-                    . . . . .
-                    . . # . .
-                    . . . . .
-                    # . . . .
-                    `)
-            } else {
-                if (Roll == 4) {
-                    basic.showLeds(`
-                        # . . . #
-                        . . . . .
-                        . . . . .
-                        . . . . .
-                        # . . . #
-                        `)
-                } else {
-                    if (Roll == 5) {
-                        basic.showLeds(`
-                            # . . . #
-                            . . . . .
-                            . . # . .
-                            . . . . .
-                            # . . . #
-                            `)
-                    } else {
-                        basic.showLeds(`
-                            # . . . #
-                            . . . . .
-                            # . . . #
-                            . . . . .
-                            # . . . #
-                            `)
-                    }
-                }
-            }
-        }
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
     }
 })
